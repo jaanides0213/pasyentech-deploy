cae40be4ed81ece46231eee9f0ab15622f19eb6d
DanicaVont <daapostol2@up.edu.ph>
2023-11-08 01:19:35 +0800

    Created routes for each tab

---
+++ src/routes/Patients.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Patients(){
+    return(
+        <div className='patients'>
+            <h1>Patients</h1>
+        </div>
+    );
+}
+
+export default Patients;
