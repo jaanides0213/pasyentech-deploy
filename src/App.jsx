6fb007acaa4037766c01be038a3b234bc5a6149f
Kikay <jaanides1@up.edu.ph>
2023-11-09 12:19:38 +0800

    /
     /

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+//import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import './App.css'
-
 import Login from "./pages/Login/Login";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Signup from "./pages/Signup/Signup"
+//import Dashboard from "./pages/Dashboard/Dashboard";
+//import Signup from "./pages/Signup/Signup";
+
+const App = () => {
+
+  
+
+}
 
 
-
-
-export default App
+export default App;
