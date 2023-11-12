8e71c5a7dffb2bfe87acf6ffce1ce0d491988a16
Kikay <jaanides1@up.edu.ph>
2023-11-12 18:20:14 +0800

    try to authenticate but failed :(

--- src/pages/Signup/SignupForm.jsx
+++ src/pages/Signup/SignupForm.jsx
@@ -1,99 +1,105 @@
-import Styles from './Signup.module.css'
+import Styles from "./Signup.module.css";
+import { useState } from "react";
 
-const SignupForm = () => {
+const SignupForm = ({ userInfo, setUserInfo }) => {
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
 
-    return (
-        <>
-            <span className={Styles["Signup_Form__span"]}>
-                <label 
-                    htmlFor="fname" 
-                    className={Styles["Signup_Form__span-label"]}
-                    name="fname"
-                >
-                    First Name
-                </label>
-                
-                <input 
-                    type='text'
-                    className={Styles["Signup_Form__span-input"]}
-                    name="fname"
-                />
-            </span>
+  return (
+    <>
+      <span className={Styles["Signup_Form__span"]}>
+        <label htmlFor="fname" className={Styles["Signup_Form__span-label"]}>
+          First Name
+        </label>
 
-            <span className={Styles["Signup_Form__span"]}>
-                <label 
-                    htmlFor="lname" 
-                    className={Styles["Signup_Form__span-label"]}
-                >
-                    Last Name
-                </label>
-                <input 
-                    type='text'
-                    className={Styles["Signup_Form__span-input"]}
-                    name="lname"
-                />
-            </span>
+        <input
+          type="text"
+          className={Styles["Signup_Form__span-input"]}
+          name="fname"
+          value={userInfo.fname}
+          onChange={handleInputChange}
+          required
+        />
+      </span>
 
-            <span className={Styles["Signup_Form__span"]}>
-                <label
-                    htmlFor="email" 
-                    className={Styles["Signup_Form__span-label"]}
-                >
-                    E-mail Address
-                </label>
-                <input 
-                    type='email'
-                    className={Styles["Signup_Form__span-input"]}
-                    name="email"
-                />
-            </span>
+      <span className={Styles["Signup_Form__span"]}>
+        <label htmlFor="lname" className={Styles["Signup_Form__span-label"]}>
+          Last Name
+        </label>
+        <input
+          type="text"
+          className={Styles["Signup_Form__span-input"]}
+          name="lname"
+          value={userInfo.lname}
+          onChange={handleInputChange}
+          required
+        />
+      </span>
 
-            <span className={Styles["Signup_Form__span"]}> {/*make sure to accept the valid 11-digit phone number (PH)*/}
-                <label
-                    htmlFor="contact" 
-                    className={Styles["Signup_Form__span-label"]}
-                >
-                    Contact Number
-                </label>
-                <input 
-                    type='text'
-                    className={Styles["Signup_Form__span-input"]}
-                    name="contactNo"
-                    pattern="[0-9]*"
-                />
-            </span>
+      <span className={Styles["Signup_Form__span"]}>
+        <label htmlFor="email" className={Styles["Signup_Form__span-label"]}>
+          E-mail Address
+        </label>
+        <input
+          type="email"
+          className={Styles["Signup_Form__span-input"]}
+          name="email"
+          value={userInfo.email}
+          onChange={handleInputChange}
+          required
+        />
+      </span>
 
-            <span className={Styles["Signup_Form__span"]}>
-                <label 
-                    htmlFor="uname" 
-                    className={Styles["Signup_Form__span-label"]}
-                >
-                    Username
-                </label>
-                <input 
-                    type='text'
-                    className={Styles["Signup_Form__span-input"]}
-                    name="uname"
-                />
-            </span>
+      <span className={Styles["Signup_Form__span"]}>
+        {" "}
+        {/*make sure to accept the valid 11-digit phone number (PH)*/}
+        <label htmlFor="contact" className={Styles["Signup_Form__span-label"]}>
+          Contact Number
+        </label>
+        <input
+          type="text"
+          className={Styles["Signup_Form__span-input"]}
+          name="contactNo"
+          pattern="[0-9]*"
+          value={userInfo.contactNo}
+          onChange={handleInputChange}
+          required
+        />
+      </span>
 
-            <span className={Styles["Signup_Form__span"]}>
-                <label 
-                    htmlFor="password" 
-                    className={Styles["Signup_Form__span-label"]}
-                >
-                    Password
-                </label>
-            
-                <input
-                    type="password"
-                    className={Styles["Signup_Form__span-input"]}
-                    name="password"
-                    minLength={8}
-                />
-            </span>
-        </>
-    );
-    }
+      <span className={Styles["Signup_Form__span"]}>
+        <label htmlFor="uname" className={Styles["Signup_Form__span-label"]}>
+          Username
+        </label>
+        <input
+          type="text"
+          className={Styles["Signup_Form__span-input"]}
+          name="uname"
+          value={userInfo.uname}
+          onChange={handleInputChange}
+          required
+        />
+      </span>
 
-export default SignupForm
+      <span className={Styles["Signup_Form__span"]}>
+        <label htmlFor="password" className={Styles["Signup_Form__span-label"]}>
+          Password
+        </label>
+
+        <input
+          type="password"
+          className={Styles["Signup_Form__span-input"]}
+          name="password"
+          minLength={8}
+          value={userInfo.password}
+          onChange={handleInputChange}
+          required
+        />
+      </span>
+    </>
+  );
+};
+
+export default SignupForm;
