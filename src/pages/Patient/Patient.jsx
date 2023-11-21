397ff2c30bab4ce02960b6bd0fc3ead9bb9fb8f4
Kikay <jaanides1@up.edu.ph>
2023-11-22 03:14:36 +0800

    try to fix

--- src/pages/Patient/Patient.jsx
+++ src/pages/Patient/Patient.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { HiSearch } from "react-icons/hi";
+import {
+  HiSearch,
+  HiOutlinePencilAlt,
+  HiOutlineEye,
+  HiOutlineTrash,
+} from "react-icons/hi";
 import { IoMdAdd } from "react-icons/io";
 import Styles from "./Patient.module.css"; // Update the import path as needed
 import Header from "../../components/Header/Header.jsx";
@@ -12,13 +17,11 @@ const Patient = () => {
     { id: 3, name: "Bob Smith", age: 28, genders: "Male" },
     // Add more sample data as needed
   ]);
-
   const [newPatient, setNewPatient] = useState({
     name: "",
     age: "",
     gender: "",
   });
-
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setNewPatient((prevPatient) => ({
@@ -26,7 +29,6 @@ const Patient = () => {
       [name]: value,
     }));
   };
-
   return (
     <main className={Styles["Patient__cont"]}>
       <Sidebar />
@@ -36,51 +38,71 @@ const Patient = () => {
         </div>
 
         <div className={Styles["Patient__cont-column-main"]}>
-          <h2 className={Styles["Patient__title"]}>Patients</h2>
-          <div className={Styles["Patient_search_add_container"]}>
-            <div className={Styles["Patient_search_bar"]}>
-              <input
-                className={Styles["Patient_search_input"]}
-                placeholder="Search patient"
-              />
-              <button className={Styles["Patient_search_button"]}>
-                <HiSearch />
-              </button>
-            </div>
-            <div className={Styles["Patient_add_bar"]}>
-              {/* <link to="/patient_form" className={Styles[]} */}
-              <button className={Styles["Patient_add_button"]}>
-                <a href="/patient_form">
-                  <IoMdAdd /> Add Patient
-                </a>
-              </button>
-            </div>
+          <h1>Patients</h1>
+        </div>
+        <div className={Styles["Patient_search_add_container"]}>
+          <div className={Styles["Patient_search_bar"]}>
+            <input
+              className={Styles["Patient_search_input"]}
+              placeholder="Search patient"
+            />
+            <button className={Styles["Patient_search_button"]}>
+              <HiSearch />
+            </button>
           </div>
-          <div>{/* <Patient_Form /> */}</div>
-          <div className={Styles["Patient_list_cont"]}>
-            <table className={Styles["Patient_table"]}>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Age</th>
-                  <th>Gender</th>
+
+          <div className={Styles["Patient_add_bar"]}>
+            {/* <link to="/patient_form" className={Styles[]} */}
+            <button className={Styles["Patient_add_button"]}>
+              <a href="/patient_form" className={Styles["Patient_add_icon"]}>
+                <IoMdAdd /> Add Patient
+              </a>
+            </button>
+          </div>
+        </div>
+        <div className={Styles["Patient_list_cont"]}>
+          <table className={Styles["Patient_table"]}>
+            <thead className={Styles["Patient_table_main_head"]}>
+              <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+
+            <tbody className={Styles["Patient_table_data"]}>
+              {patients.map((patient) => (
+                <tr key={patient.id}>
+                  <td>{patient.name}</td>
+                  <td>{patient.age}</td>
+                  <td>{patient.gender}</td>
+                  <td>
+                    <div className={Styles["Patient_table_action"]}>
+                      <div className={Styles["Action__Styling"]}>
+                        <a href="#" className={Styles["Action__link__Styling"]}>
+                          <HiOutlineEye size="15px" /> View
+                        </a>
+                      </div>
+                      <div className={Styles["Action__Styling"]}>
+                        <a href="#" className={Styles["Action__link__Styling"]}>
+                          <HiOutlinePencilAlt size="15px" /> Edit
+                        </a>
+                      </div>
+                      <div className={Styles["Action__Styling"]}>
+                        <a href="#" className={Styles["Action__link__Styling"]}>
+                          <HiOutlineTrash size="15px" /> Delete
+                        </a>
+                      </div>
+                    </div>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {patients.map((patient) => (
-                  <tr key={patient.id}>
-                    <td>{patient.name}</td>
-                    <td>{patient.age}</td>
-                    <td>{patient.gender}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </main>
   );
 };
-
 export default Patient;
