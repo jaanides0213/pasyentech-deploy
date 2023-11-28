352d59fb78b951103d2443a1ec2d881a6588154e
Kikay <jaanides1@up.edu.ph>
2023-11-28 14:57:45 +0800

    Update Dashboard.jsx

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -7,6 +7,7 @@ import { IoMdAdd } from "react-icons/io";
 import Header from "../../Components/Header/Header.jsx";
 import { getUserData } from "../../api/getUserData";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { Sidebar } from "react-pro-sidebar";
 
 const Dashboard = ({ userData }) => {
   const [user, setUser] = useState(userData);
@@ -45,6 +46,7 @@ const Dashboard = ({ userData }) => {
   return (
     <main className={Styles["Dashboard__cont"]}>
       <div className={Styles["Dashboard__cont-main"]}>
+        <Sidebar />
         <div className={Styles["Dashboard__cont-header"]}>
           <Header />
         </div>
@@ -67,14 +69,12 @@ const Dashboard = ({ userData }) => {
           <span className={Styles["Dashboard__shortcut-column"]}>
             <h3>Shortcuts:</h3>
             <button className={Styles["Dashboard__cont-add_patient"]}>
-              <a href="/patientform">
-                <IoMdAdd /> Add Patient
-              </a>
+              <IoMdAdd />
+              Add Patient
             </button>
             <button className={Styles["Dashboard__cont-add_prescription"]}>
-              <a href="#">
-                <IoMdAdd /> Add Prescription
-              </a>
+              <IoMdAdd />
+              Add Prescription
             </button>
           </span>
         </div>
