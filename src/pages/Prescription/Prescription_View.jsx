fb88c03addf0297d88222252820f3c7c64742025
Jesiell Añides <jaanides1@up.edu.ph>
2024-01-06 19:32:22 +0800

    Update Prescription_View.jsx

--- src/pages/Prescription/Prescription_View.jsx
+++ src/pages/Prescription/Prescription_View.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useEffect,useState} from 'react'
 import Styles from "./Prescription.module.css";
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Header from '../../components/Header/Header';
 import { HiOutlinePrinter } from "react-icons/hi";
 import { getPrescriptionById } from '../../api/getPrescriptionById';
+import { useParams } from "react-router-dom";
 
 const Prescription_View = () => {  
+
+  const { id } = useParams();
+  const [prescription, setPrescription] = useState(null);
+
+  useEffect(() => {
+    const fetchPrescription = async () => {
+      try {
+        const fetchedPrescription = await getPrescriptionById(id);
+        setPrescription(fetchedPrescription);
+        console.log("Fetched Prescription:", fetchedPrescription);
+      } catch (error) {
+        console.error("Error fetching prescription:", error);
+      }
+    };
+
+    fetchPrescription();
+  }, [id]);
+
   return (
     <main className={Styles["Prescription__cont"]}>
       <Sidebar/>
@@ -20,34 +39,38 @@ const Prescription_View = () => {
           <hr className={Styles["Prescription__hr"]}/>
         </div>
 
+        {prescription && (
         <div className={Styles["Prescription__patientInfo__container"]}>
           <div className={Styles["Prescription__patientInfo__span"]}>
             <h3>Patient Information</h3>
-            <p>Name of Patient: </p>
-            <p>Age: </p>
-            <p>Sex: </p>
-            <p>Weight: </p>
-            <p>Address: </p>
-            <p>Consultation date: </p>
+            <p>Name of Patient: {prescription.patientName}</p>
+            <p>Age: {prescription.patientAge}</p>
+            <p>Sex: {prescription.patientSex}</p>
+            <p>Weight: {prescription.patientWeight}</p>
+            <p>Address: {prescription.patientAddress}</p>
+            <p>Consultation date: {prescription.patientConsultationDate}</p>
           </div> 
 
           <div className={Styles["Prescription__patientInfo__span1"]}>
             <h3>Medications</h3>
-            <div>
-              <p>Dosage: </p>
-              <p>Unit: </p>
-              <p>Generic Name: </p>
-              <p>Brand Name: </p>
-              <p>Direction of Use: </p>
-            </div>
-          </div> 
+            {prescription.medications.map((medication, index) => (
+              <div key={index}>
+                <p>Dosage: {medication.dosageNum} {medication.dosageUnit}</p>
+                <p>Generic Name: {medication.genericName}</p>
+                <p>Brand Name: {medication.brandName}</p>
+                <p>Direction of Use: {medication.directionOfUse}</p>
+              </div>
+            ))}
+          </div>
         </div>
+        )}
 
         <div className={Styles["Prescription__print__container"]}>
             <a href='#'>
               <HiOutlinePrinter size="1.1rem"/> Print
             </a>            
           </div>
+          
       </div>     
     </main>
   )
