68a3d1ec082b8e51bce1426a6317761b4688ae66
Jesiell Añides <jaanides1@up.edu.ph>
2024-01-06 20:20:23 +0800

    Update Appointment view

--- src/pages/Appointment/Appointment_View.jsx
+++ src/pages/Appointment/Appointment_View.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import Styles from "./Appointment.module.css";
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Header from '../../components/Header/Header';
+import { useParams } from 'react-router';
+import {getAppointmentById} from "../../api/getAppointmentById";
+
 
 const Appointment_View = () => {
+
+  const { id } = useParams();
+  const [appointment, setAppointment] = useState(null);
+
+  useEffect(() => {
+    const fetchAppointment = async () => {
+      try {
+        const fetchedAppointment = await getAppointmentById(id);
+        setAppointment(fetchedAppointment);
+        console.log("Fetched Appointment:", fetchedAppointment);
+      } catch (error) {
+        console.error("Error fetching Appointment:", error);
+      }
+    };
+
+    fetchAppointment();
+  }, [id]);
+
   return (
     <main className={Styles["Appointment__cont"]}>
       <Sidebar/>
@@ -18,14 +39,15 @@ const Appointment_View = () => {
             <hr className={Styles["Appointment__hr"]}/>
           </div>
 
+          {appointment &&(
           <div className={Styles["Appointment__info__container"]}>
             <h3>Appointment Details</h3>
-            <p>Name of Patient: </p>
-            <p>Age: </p>
-            <p>Date: </p>
-            <p>Status: </p>
-            <p>Remarks (if any): </p>
-          </div>
+            <p>Name of Patient: {appointment.patientName}</p>
+            <p>Age: {appointment.apptAge}</p>
+            <p>Date: {appointment.apptDate}</p>
+            <p>Status: {appointment.apptStatus}</p>
+            <p>Remarks (if any): {appointment.apptRemark}</p>
+          </div>)}
         </div>
     </main>
   )
