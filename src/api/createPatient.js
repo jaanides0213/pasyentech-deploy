3f8a8617c5edca6a746803af13e4aa19914b3318
kgpelayo <105369973+kgpelayo@users.noreply.github.com>
2024-01-09 14:43:36 +0800

    FIXED EDIT/ADD PATIENT
    putanginaaaaaaaaaaaaa

--- src/api/createPatient.js
+++ src/api/createPatient.js
@@ -6,6 +6,7 @@ import {
   doc,
 } from "firebase/firestore";
 import { auth, db } from "../config/firebase";
+import { getPatientById } from "./getPatientById";
 
 export const createPatient = async (newPatient, currentUser) => {
   // You can use the currentUser information if needed for any specific logic
@@ -20,11 +21,27 @@ export const createPatient = async (newPatient, currentUser) => {
   console.log("hahaha", newPatient);
   const patientId = newPatient.patientId;
 
+  if (getPatientById(patientId)){
+    try {
+      const patientRef = doc(db, "patients", patientId);
+      await setDoc(patientRef, {
+        ...newPatient
+      })
+  
+      console.log("Patient added successfully: ", patientRef.id);
+  
+      return patientRef.id;
+    } catch (error) {
+      console.error("error: ", error);
+    }
+  }
+  
   // If patientId is undefined or empty, remove it from the object
   if (!patientId || patientId === "") {
     // This ensures that patientId is not sent as an undefined or empty string
     delete newPatient.patientId;
   }
+
 
   try {
     const patientRef = await addDoc(collection(db, "patients"), {
